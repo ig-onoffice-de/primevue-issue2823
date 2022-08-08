305879d6eda67d4e5cd6ab57c7721681bed81478
Isabelle Glasmacher <i.glasmacher@onoffice.de>
2022-08-08 17:05:53 +0200

    remove boilerplate and add menu

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Menu from 'primevue/menu'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('Menu', Menu)
+
+
+app.mount('#app')
